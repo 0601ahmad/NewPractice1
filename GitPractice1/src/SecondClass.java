1d88f3d01dc4bdeaef98f85e358adca1662c7e58
0601ahmad <ghafooriahmadfarhad@gmail.com>
2022-01-05 21:05:02 -0800

    new class

---
+++ GitPractice1/src/SecondClass.java
@@ -0,0 +1,23 @@
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		int [] num = {3,5,7,4,3,7,9};
+		
+		for (int i=0;i<num.length-1;i++) {
+			for (int j=0;j<num.length-1;j++) {
+				if (num[j]>num[j+1]) {
+					int temp = num[j];
+					num[j]= num[j+1];
+					num[j+1] = temp;
+				}
+			}
+			
+		}
+		for (int i: num) {
+			System.out.println(i);
+		}
+
+	}
+
+}
