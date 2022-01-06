f3feddcc00270122c3907d54b4397c54c83d51c1
0601ahmad <ghafooriahmadfarhad@gmail.com>
2022-01-05 20:55:04 -0800

    more changes

--- GitPractice1/src/FirstClass.java
+++ GitPractice1/src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		String name = "Ahmad Farhad";
 		String lastname = "Ghafoori";
+		System.out.println("My FullName: "+name+" "+lastname);
 
 	}
 
